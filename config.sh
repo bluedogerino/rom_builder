725b9608e77c7fbc9a26f79aced6b48952ea8dc1
Griffin <bluedogerino@gmail.com>
2019-07-24 02:48:29 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-GITHUB_USER=ExpressLuke
-GITHUB_EMAIL=expressluke@turbox.uk
+GITHUB_USER=Griffin
+GITHUB_EMAIL=bluedogerino@gmail.com
 
-KBUILD_BUILD_USER="Luke"
-KBUILD_BUILD_HOST="ExpressDrone"
+KBUILD_BUILD_USER="griffin"
+KBUILD_BUILD_HOST="BlueDrone"
 
-export oem=motorola
-export device=surnia
+export oem=samsung
+export device=j5y17lte
 
-ROM="PixelExperience"
-manifest_url="https://github.com/PixelExperience/manifest"
-export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
-branch="pie"
+ROM="LineageOS"
+manifest_url="https://github.com/LineageOS/android"
+export rom_vendor_name="lineage" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
+branch="lineage-16.0"
 
 release_repo="ExpressLuke/github-releases"
 
