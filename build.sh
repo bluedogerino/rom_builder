2c5fe7722309af6a587f710ab8e8608efecd9c85
Griffin <bluedogerino@gmail.com>
2019-07-25 14:47:14 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -73,8 +73,7 @@ Build Started: [See Progress]("$ci_url")"
 
     export finalzip_path=$(ls "$outdir"/*201*.zip | tail -n -1)
     export zip_name=$(echo "$finalzip_path" | sed "s|"$outdir"/||")
-    export zip_time=$(echo "date -r "$outdir"/*201*.zip +%H-%M")
-    export tag=$( echo "$zip_name-$zip_time" | sed 's|.zip||')
+    export tag=$( echo "$zip_name" | sed 's|.zip||')
     if [ -e "$finalzip_path" ]; then
         echo "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
 
